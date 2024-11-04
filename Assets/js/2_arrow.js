ecfdada79fdd1a868d53f3ffde16bd0dcaefed22
Michelle Valdés <michelle.vlds1997@gmail.com>
2024-11-04 13:36:45 -0300

    ejercicio 1 y 2

---
+++ Assets/js/2_arrow.js
@@ -0,0 +1,7 @@
+//Transforma la siguiente función a una arrow function de una línea.
+let suma = function(a, b){
+    return a + b
+    }
+
+//respuesta
+suma = (a, b) => a + b
---
+++ Assets/js/script.js
@@ -0,0 +1,15 @@
+//Funcion para pintar original
+/*function pintar(){
+    ele.style.backgroundColor = 'yellow'
+    }
+    const ele = document.getElementById("ele1")
+    ele.addEventListener("click", pintar);*/
+    
+//Respuesta
+function pintar(elemento, color = 'green') {
+    elemento.style.backgroundColor = color;
+    }
+    const ele = document.getElementById("ele1")
+    ele.addEventListener("click", function() { 
+        pintar(this, 'yellow');
+    });
